4a89585cbf937a7cc8a23a727101ebb6623ac965
francesco <francesco.penasa-1@studenti.unitn.it>
2018-07-27 21:46:16 +0200

    4-11 e 4-12 non completi

---
+++ pcc/4-11.My_Pizzas_Your_Pizzas.py
@@ -0,0 +1,10 @@
+# My Pizzas, Your Pizzas
+#
+# Start with your program from 4-1.
+# Make a coppy of the list of pizzas, and call it friend_pizzas.
+# Then, do the following:
+# - Add a new pizza to the original list 
+# - Add a different pizza to the list friend_pizzas
+# - Prove that you have two separate lists. 
+#   Print the message, My favorite pizzas are:, and then use a for loop to print the first list.
+#   Make sure each new pizza is stored in the appropriate list,
